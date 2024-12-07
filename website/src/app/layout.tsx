f42d626fd5e033b226df8c7e063110862754213a
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2024-12-07 14:15:13 +0900

    Header menu setting

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "@/style/styles.scss"
+import "@/styles/style.scss"
 
 
 const geistSans = localFont({
---
+++ website/src/components/Header/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopNav from './TopNav/page';
+import Menu from './Menu/page';
+
+
+const Header = () => {
+  return (
+    <header id="header">
+      <TopNav />
+      <Menu />
+    </header>
+  );
+};
+
+export default Header;
