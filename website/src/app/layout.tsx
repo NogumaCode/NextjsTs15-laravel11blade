d78e6bca948d32ebe06ef0140964ddc710d299d8
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2024-12-07 18:04:00 +0900

    slider setting

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "@/styles/style.scss"
 
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
