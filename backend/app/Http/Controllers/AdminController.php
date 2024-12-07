3923ee518c34a8579c6b220d6b51b60d4093496a
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2024-12-07 12:44:40 +0900

    frond nextjs install and backend login and logout function setting

---
+++ backend/app/Http/Controllers/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function AdminLogout(Request $request)
+    {
+        // 管理者用ガードでログアウト
+        Auth::guard('web')->logout();
+        // セッションを無効化
+        $request->session()->invalidate();
+        // CSRFトークンを再生成
+        $request->session()->regenerateToken();
+
+        // ログイン画面へリダイレクト
+        return redirect('/login');
+    }
+}
