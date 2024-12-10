91b432549029e4e358c9e2934a57ad0a68915b75
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2024-12-10 21:43:02 +0900

    payment part setting

--- website/src/components/payment_gateway/page.tsx
+++ website/src/components/payment_gateway/page.tsx
@@ -2,6 +2,8 @@
 import { useInView } from "framer-motion";
 import { useRef } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { Phone } from "@phosphor-icons/react/dist/ssr";
 
 function PaymentGateway() {
   const ref = useRef<HTMLDivElement | null>(null);
@@ -28,7 +30,33 @@ function PaymentGateway() {
                   opacity: isInView ? 1 : 0,
                   transition: "all 0.7s cubic-bezier(0.17,0.55,0.55,1) 0.3s",
                 }}
-              ></div>
+              >
+                <div className="heading flex items-center gap-4 max-lg:flex-wrap">
+                  <div className="flex items-center">
+                    <div className="img sm:w-12 w-10 sm:h-12 h-10 rounded-full overflow-hidden bg-line p-0 z-[3]">
+                      <Image className="full h-full rounded-full" width={300} height={300} src='/images/avatar3.webp' alt="img" />
+                    </div>
+                  </div>
+                <div className="text-button">Trusted by 5M+ People<br/>Around the glove </div>
+                </div>
+                <div className="text lg:mt-14 mt-5">
+                  <h3 className="heading3">Payment Gateway Services</h3>
+                  <div className="body3 text-secondary lg:mt-6 mt-4">
+                    Get personalized financial advice to help reach your financial goals. Get personalized financial advice to help reach your financial goals.
+                  </div>
+                </div>
+                <div className="button-block flex items-center max-sm:flex-wrap sm:gap-6 gap-3 lg:mt-12 mt-8 w-fit">
+                  <Link className="button-main box-shadow bg-blue-700 hover:bg-bl text-white bg-blue whitespace-nowrap rounded-full" href="/">
+                  Get Started
+                  </Link>
+                  <div className="relative">
+                    <Link className="button-main box-shadow hover:bg-black hover:text-white text-on-surface bg-white flex items-center gap rounded-full relative z-[1]" href='/'>
+                    <Phone weight='fill' className="text-xl" />
+                    <span className="whitespace-nowrap">(00) 123 123 456</span></Link>
+                    <Image src='/images/component/gateway1-dot.png' className="absolute -right-12 w-[100px] h-auto top-1/2 -translate-y-1/2" width={4000} height={4000} alt='img' />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
       </section>
