8fa3935c8511712526e320147eb65e3aa0583faf
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2024-12-17 12:15:48 +0900

    service function setting

---
+++ backend/app/Http/Controllers/Backend/ServiceController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class ServiceController extends Controller
+{
+     public function AllService(){
+    	$service = Service::latest()->get();
+    	return view('backend.service.all_service',compact('service'));
+    }
+    // End Method
+}
